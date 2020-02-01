793fdb7b3a80fb5b3db2e034b1b20a379938547f
theo <theo>
2020-02-01 19:29:30 +0100

    edit: sodastream steht in raum2 und zerfällt

---
+++ Assets/Scripts/SodaStreamSammelstück.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+
+public class SodaStreamSammelstück : MonoBehaviour
+{
+    private WorkBeench workbench;
+    private Interactable sodaInteractabale;
+    // Start is called before the first frame update
+    void Start()
+    {
+        workbench = FindObjectOfType<WorkBeench>();
+        sodaInteractabale = GetComponent<Interactable>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (sodaInteractabale.isHovering)
+        {
+            workbench.sodaWasFound = true;
+        }
+    }
+}
