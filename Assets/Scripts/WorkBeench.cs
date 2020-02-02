aac5615a0417a471e56f4bce3d8155cd4958fdae
theo <theo>
2020-02-02 15:58:10 +0100

    edit: scene_naughty_boiplayer ist fertig

--- Assets/Scripts/WorkBeench.cs
+++ Assets/Scripts/WorkBeench.cs
@@ -9,14 +9,23 @@ public class WorkBeench : MonoBehaviour
     public GameObject Checkmark; // our checkmark prefab
 
     public bool clockWasFound;
+    public bool clockAlreadyBuilt;
     public bool sodaWasFound;
+    public bool sodaAlreadyBuilt;
     public bool erlenmeyerWasFound;
+    public bool erlenmeyerAlreadyBuilt;
     public bool doorHandleWasFound;
+    public bool doorHandleAlreadyBuilt;
     public bool pipeWasFound;
-    /*public bool batteryWasFound;
+    public bool pipeAlreadyBuilt;
+    public bool batteryWasFound;
+    public bool batteryWasBuilt;
     public bool prismWasFound;
+    public bool prismAlreadyBuilt;
     public bool potWasFound;
-    public bool cableWasFound;*/
+    public bool potAlreadyBuilt;
+    public bool cableWasFound;
+    public bool cableAlreadyBuilt;
 
     public bool alreadyBuilt;
     public GameObject finishedGlueGun;
@@ -25,7 +34,7 @@ public class WorkBeench : MonoBehaviour
 
     public GameObject Clock;
     public GameObject ClockPointer1;
-    public GameObject Clockpointer2;
+    public GameObject ClockPointer2;
 
     public GameObject Soda;
     public GameObject ErlenMeyer;
@@ -42,15 +51,26 @@ public class WorkBeench : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Checkmark.transform.position -= new Vector3(-Checkmark.transform.position.x, 0, -Checkmark.transform.position.z);
+
         clockWasFound = false;
+        clockAlreadyBuilt = false;
+        sodaAlreadyBuilt = false;
         sodaWasFound = false;
         erlenmeyerWasFound = false;
+        erlenmeyerAlreadyBuilt = false;
         doorHandleWasFound = false;
+        doorHandleAlreadyBuilt = false;
         pipeWasFound = false;
-        /*batteryWasFound = false;
+        pipeAlreadyBuilt = false;
+        batteryWasFound = false;
+        batteryWasBuilt = false;
         prismWasFound = false;
+        prismAlreadyBuilt = false;
         potWasFound = false;
-        cableWasFound = false;*/
+        potAlreadyBuilt = false;
+        cableWasFound = false;
+        cableAlreadyBuilt = false;
 
         alreadyBuilt = false;
     }
@@ -65,64 +85,89 @@ public class WorkBeench : MonoBehaviour
             Instantiate(finishedGlueGun, transform.position, finishedGlueGun.transform.rotation);
         }
 
-        if (clockWasFound)
+        if (clockWasFound && !clockAlreadyBuilt)
         {
+            clockAlreadyBuilt = true;
             GameObject checkmark = Instantiate(Checkmark, Checkmark.transform);
             checkmark.transform.position += new Vector3(17.854f, 0, 1.511f);
             Clock.SetActive(true);
             ClockPointer1.SetActive(true);
             ClockPointer2.SetActive(true);
-
         }
 
-        if (sodaWasFound)
+        if (sodaWasFound && !sodaAlreadyBuilt)
         {
+            sodaAlreadyBuilt = true;
             GameObject checkmark = Instantiate(Checkmark, Checkmark.transform);
             checkmark.transform.position += new Vector3(18.3446f, 0, 1.3818f);
+            Soda.SetActive(true);
         }
 
-        if (erlenmeyerWasFound)
+        if (erlenmeyerWasFound && !erlenmeyerAlreadyBuilt)
         {
+            erlenmeyerAlreadyBuilt = true;
             GameObject checkmark = Instantiate(Checkmark, Checkmark.transform);
             checkmark.transform.position += new Vector3(18.0409f, 0, 1.4707f);
+            ErlenMeyer.SetActive(true);
         }
 
-        if (doorHandleWasFound)
+        if (doorHandleWasFound && !doorHandleAlreadyBuilt)
         {
             GameObject checkmark = Instantiate(Checkmark, Checkmark.transform);
             checkmark.transform.position += new Vector3(17.7911f, 0, 1.0704f);
+            doorHandleAlreadyBuilt = true;
+            DoorHandle.SetActive(true);
         }
 
-        if (pipeWasFound)
+        if (pipeWasFound && !pipeAlreadyBuilt)
         {
+            pipeAlreadyBuilt = true;
             GameObject checkmark = Instantiate(Checkmark, Checkmark.transform);
             checkmark.transform.position += new Vector3(17.8435f, 0, 1.3742f);
+            Pipe.SetActive(true);
         }
-        /*
-        if (prismWasFound)
+        
+        if (prismWasFound && !prismAlreadyBuilt)
         {
-            GameObject checkmark = Instantiate(checkmark, checkmark.position);
-            checkmark.transform.position + new Vector3(17.6937f, 0, 1.2254f);
+            prismAlreadyBuilt = true;
+            GameObject checkmark = Instantiate(Checkmark, Checkmark.transform);
+            checkmark.transform.position += new Vector3(17.6937f, 0, 1.2254f);
+            Prism.SetActive(true);
+            
         }
 
-        if (potWasFound)
+        if (potWasFound && !potAlreadyBuilt)
         {
-            GameObject checkmark = Instantiate(checkmark, checkmark.position);
-            checkmark.transform.position + new Vector3(17.9772f, 0, 1.1956f);
+            potAlreadyBuilt = true;
+            GameObject checkmark = Instantiate(Checkmark, Checkmark.transform);
+            checkmark.transform.position += new Vector3(17.9772f, 0, 1.1956f);
+            Pot.SetActive(true);
+            
         }
 
-        if (cableWasFound)
+        if (cableWasFound && !cableAlreadyBuilt)
         {
-            GameObject checkmark = Instantiate(checkmark, checkmark.position);
-            checkmark.transform.position + new Vector3(18.3484f, 0, 1.391f);
+            cableAlreadyBuilt = true;
+            GameObject checkmark = Instantiate(Checkmark, Checkmark.transform);
+            checkmark.transform.position += new Vector3(18.3484f, 0, 1.391f);
+            Cable.SetActive(true);
+            CableConnectors.SetActive(true);
         }
-        */
-        if (clockWasFound && sodaWasFound && erlenmeyerWasFound && doorHandleWasFound && pipeWasFound)
+        if (batteryWasFound && !batteryWasBuilt)
+        {
+            batteryWasBuilt = true;
+            GameObject checkmark = Instantiate(Checkmark, Checkmark.transform);
+            checkmark.transform.position += new Vector3(18.3484f, 0, 1.391f);
+            Battery.SetActive(true);
+        }
+
+
+        /*if (clockWasFound && sodaWasFound && erlenmeyerWasFound && doorHandleWasFound && pipeWasFound)
         {
             alreadyBuilt = true;
             SpawnGun();
             
-        }
+        }*/
     }
 
     void SpawnGun()
