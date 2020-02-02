95fbe45f68644e6b2a3bedc9e5a74a9d4af5a363
theo <theo>
2020-02-02 12:16:45 +0100

    basketball in room_2

---
+++ Assets/Scripts/DoorhandleTeleport.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorhandleTeleport : MonoBehaviour
+{
+    public Transform vaseRoom1;
+    public Transform kitchenRoom2;
+    public Transform workBenchRoom3;
+
+    public float teleportOffset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void TeleportPlayer()
+    {
+        vaseRoom1.transform.position = new Vector3(vaseRoom1.transform.position.x + teleportOffset, vaseRoom1.transform.position.y, vaseRoom1.transform.position.z);
+        kitchenRoom2.transform.position = new Vector3(kitchenRoom2.transform.position.x + teleportOffset, kitchenRoom2.transform.position.y, kitchenRoom2.transform.position.z);
+        workBenchRoom3.transform.position = new Vector3(workBenchRoom3.transform.position.x + teleportOffset, workBenchRoom3.transform.position.y, workBenchRoom3.transform.position.z);
+    }
+}
